f93cc34fb5ae91ffac424a328e9d6d35260a867e
kajgaard <maria.kajgaard@gmail.com>
2020-03-25 10:36:08 +0100

    Added elements in Main Activity

--- app/src/main/java/com/cdio/ss3000/MainActivity.java
+++ app/src/main/java/com/cdio/ss3000/MainActivity.java
@@ -2,13 +2,43 @@ package com.cdio.ss3000;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button start, help;
+    TextView header;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start = findViewById(R.id.startBtn);
+        start.setOnClickListener(this);
+
+        help.findViewById(R.id.helpBtn);
+        help.setOnClickListener(this);
+
+        header.findViewById(R.id.headerTv);
+
+
+
     }
-            }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v == start){
+            Intent i = new Intent(this,CameraActivity.class);
+            this.startActivity(i);
+        }else if (v == help){
+            //Intent i = new Intent(this,)
+        }
+    }
+}
+
+
