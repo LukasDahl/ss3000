b6100bc54fc283836819015ca5879437779f8d0e
Søren Klose <klose29@live.dk>
2020-04-15 17:55:20 +0200

    made early shared preferences

--- app/src/main/java/com/cdio/ss3000/HighscoreActivity.java
+++ app/src/main/java/com/cdio/ss3000/HighscoreActivity.java
@@ -2,13 +2,32 @@ package com.cdio.ss3000;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 public class HighscoreActivity extends AppCompatActivity {
 
+  String[] hsNameList = new String[10];
+  int[] hsScoreList = new int[10];
+
+  SharedPreferences prefName, prefScore;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    prefName = getSharedPreferences("highscoreName", Context.MODE_PRIVATE);
+    prefScore = getSharedPreferences("highscoreScore", Context.MODE_PRIVATE);
+
+    for (int i = 0; i < 10; i++){
+      hsNameList[i] = prefName.getString(""+i, "Null");
+      hsScoreList[i] = prefScore.getInt(""+i, 0);
+    }
+
+    SharedPreferences.Editor nameEditor = prefName.edit();
+    SharedPreferences.Editor scoreEditor = prefScore.edit();
+
     setContentView(R.layout.activity_highscore);
   }
 }
