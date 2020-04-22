b1c545df3926c3d23180333069ccfbd1b8411b8e
Søren Klose <klose29@live.dk>
2020-04-22 11:27:02 +0200

    !!! NOT WORKING YET !!! Started the recyclerview for the highscore list

--- app/src/main/java/com/cdio/ss3000/HighscoreActivity.java
+++ app/src/main/java/com/cdio/ss3000/HighscoreActivity.java
@@ -1,10 +1,15 @@
 package com.cdio.ss3000;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class HighscoreActivity extends AppCompatActivity {
 
@@ -13,9 +18,13 @@ public class HighscoreActivity extends AppCompatActivity {
 
   SharedPreferences prefName, prefScore;
 
+  RecyclerView recyclerView;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    recyclerView = findViewById(R.id.recyclerView);
 
     prefName = getSharedPreferences("highscoreName", Context.MODE_PRIVATE);
     prefScore = getSharedPreferences("highscoreScore", Context.MODE_PRIVATE);
@@ -28,6 +37,34 @@ public class HighscoreActivity extends AppCompatActivity {
     SharedPreferences.Editor nameEditor = prefName.edit();
     SharedPreferences.Editor scoreEditor = prefScore.edit();
 
+    recyclerView.setAdapter(adapter);
+
     setContentView(R.layout.activity_highscore);
+
   }
+
+  RecyclerView.Adapter adapter = new RecyclerView.Adapter() {
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+      View itemView = getLayoutInflater().inflate(R.layout.highscore_element, parent, false);
+      return new RecyclerView.ViewHolder(itemView){};
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+      TextView hsNumber = holder.itemView.findViewById(R.id.hsNumber);
+      TextView hsName = holder.itemView.findViewById(R.id.hsName);
+      TextView hsScore = holder.itemView.findViewById(R.id.hsScore);
+
+      hsNumber.setText("" + position + ".");
+      hsName.setText("" + hsNameList[position]);
+      hsScore.setText("" + hsScoreList[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+      return hsNameList.length;
+    }
+  };
 }
