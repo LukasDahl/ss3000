7a00bea0ba299892cb19ea1db894e6f2c9c943df
kajgaard <maria.kajgaard@gmail.com>
2020-04-29 11:34:58 +0200

    Added Imageview to helppage

--- app/src/main/java/com/cdio/ss3000/HelpActivity.java
+++ app/src/main/java/com/cdio/ss3000/HelpActivity.java
@@ -3,12 +3,21 @@ package com.cdio.ss3000;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
 
 public class HelpActivity extends AppCompatActivity {
+
+    TextView helptext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+
+        helptext = findViewById(R.id.brodtextTV);
+       helptext.setMovementMethod(new ScrollingMovementMethod());
     }
 }
