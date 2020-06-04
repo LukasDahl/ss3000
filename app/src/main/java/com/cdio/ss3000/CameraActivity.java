ac51367a7304e938d8efbe617899fcd24ac37bc8
kajgaard <maria.kajgaard@gmail.com>
2020-06-04 13:05:29 +0200

    Added button to CameraActivity

--- app/src/main/java/com/cdio/ss3000/CameraActivity.java
+++ app/src/main/java/com/cdio/ss3000/CameraActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 
-public class CameraActivity extends AppCompatActivity {
+public class CameraActivity extends AppCompatActivity implements View.OnClickListener {
 
+  Button capture;
   Camera camera;
   FrameLayout framelayout;
   ShowCamera showCamera;
@@ -19,10 +22,20 @@ public class CameraActivity extends AppCompatActivity {
     setContentView(R.layout.activity_camera);
     framelayout = (FrameLayout)findViewById(R.id.frameLayout);
 
+    capture = findViewById(R.id.captureBtn);
+    capture.setOnClickListener(this);
+
     //opens the camera
     camera = Camera.open();
 
     showCamera = new ShowCamera(this, camera);
     framelayout.addView(showCamera);
+  }
+
+  @Override
+  public void onClick(View v) {
+    if(v == capture){
+
+    }
   }
 }
