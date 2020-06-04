439e7e607fa6fd96a182dd16e037afa1afb7f1e1
kajgaard <maria.kajgaard@gmail.com>
2020-06-04 14:00:14 +0200

    DOES NOT WORK. Saving bitmaps in Arraylist CarmeraActivity

--- app/src/main/java/com/cdio/ss3000/CameraActivity.java
+++ app/src/main/java/com/cdio/ss3000/CameraActivity.java
@@ -2,19 +2,26 @@ package com.cdio.ss3000;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.hardware.Camera;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 
 public class CameraActivity extends AppCompatActivity implements View.OnClickListener {
 
+  ImageView image;
   Button capture;
   Camera camera;
   FrameLayout framelayout;
   ShowCamera showCamera;
+  ArrayList<Bitmap> bmArray = new ArrayList<Bitmap>();
+  Bitmap bm = null;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +29,14 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
     setContentView(R.layout.activity_camera);
     framelayout = (FrameLayout)findViewById(R.id.frameLayout);
 
+
+    //FrameLayout framelayout;
+
     capture = findViewById(R.id.captureBtn);
     capture.setOnClickListener(this);
+
+    image = findViewById(R.id.testiv);
+    image.setImageBitmap(null);
 
     //opens the camera
     camera = Camera.open();
@@ -36,6 +49,17 @@ public class CameraActivity extends AppCompatActivity implements View.OnClickLis
   public void onClick(View v) {
     if(v == capture){
 
+      for(int i=0; i<10; i++){
+        framelayout = (FrameLayout)findViewById(R.id.frameLayout);
+        framelayout.setDrawingCacheEnabled(true);
+        framelayout.buildDrawingCache();
+        bm = framelayout.getDrawingCache();
+        bmArray.add(bm);
+        framelayout.destroyDrawingCache();
+        System.out.println("Array er: "+bmArray.isEmpty());
+
+      }
+      image.setImageBitmap(bmArray.get(2));
     }
   }
 }
