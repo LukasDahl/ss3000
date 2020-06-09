aca929fb0b393d87de1aac0fadfd729ad0a6f07e
Lucas Jean Louis Gramme <s164863@win.dtu.dk>
2020-06-09 10:59:37 +0200

    Added start to point calculator

--- app/src/main/java/com/cdio/ss3000/DataLayer/PointCalculator.java
+++ app/src/main/java/com/cdio/ss3000/DataLayer/PointCalculator.java
@@ -1,6 +1,27 @@
 package com.cdio.ss3000.DataLayer;
 
+import java.util.LinkedList;
+
 public class PointCalculator {
+
+    /*
+    Value of empty slot in tableu is -1
+    Value of empty slot in foundation is -2
+     */
+    private Card checkMoves(LinkedList<LinkedList> listOfMoves){
+        if(listOfMoves.isEmpty()) {
+            return null;
+        }else{
+            for(LinkedList lists : listOfMoves){
+                Card card = (Card)lists.peek();
+                if(card.getPlacedInFoundation()){
+                    return card;
+                }
+                //TODO: If it is not a move to tableu do something?
+            }
+        }
+        return null;
+    }
 
     public Card getMoveWithMostPoint(Card card){
         return new Card();
