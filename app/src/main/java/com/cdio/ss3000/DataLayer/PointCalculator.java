b96c3021643215907839d63bba50954f63b51dcb
Flotfyr27 <42938246+Flotfyr27@users.noreply.github.com>
2020-06-09 11:33:05 +0200

    Added giving points for moves

--- app/src/main/java/com/cdio/ss3000/DataLayer/PointCalculator.java
+++ app/src/main/java/com/cdio/ss3000/DataLayer/PointCalculator.java
@@ -8,19 +8,35 @@ public class PointCalculator {
     Value of empty slot in tableu is -1
     Value of empty slot in foundation is -2
      */
-    private Card checkMoves(LinkedList<LinkedList> listOfMoves){
-        if(listOfMoves.isEmpty()) {
-            return null;
-        }else{
-            for(LinkedList lists : listOfMoves){
-                Card card = (Card)lists.peek();
-                if(card.getPlacedInFoundation()){
-                    return card;
-                }
-                //TODO: If it is not a move to tableu do something?
+    private Card checkMoves(Card card){
+        int temp_points = 0;
+        int i = -1;
+        for(Card move : card.getMoves()){
+            ++i;
+            //Tableu
+            if(move.getValue() == -1 && move.getSuit() == Suit.UNKNOWN){
+                temp_points += 3;
+            }
+            //Foundation
+            if(move.getValue() == -2 && move.getSuit() == Suit.UNKNOWN){
+                temp_points += 5;
+            }
+            if(move.getValue() > 0 && move.getValue() < 14){
+                temp_points += 2;
+                //Add points for each underlying face down card
+            }
+            move.setPoints(temp_points);
+        }
+        Card newCard = null;
+        int highestPoint = 0;
+        for(int j = 0; j < card.getMoves().size(); j++){
+            if(card.getMoves().get(j).getPoints() > highestPoint){
+                highestPoint = card.getMoves().get(j).getPoints();
+                newCard = card.getMoves().get(j);
             }
         }
-        return null;
+
+        return newCard;
     }
 
     public Card getMoveWithMostPoint(Card card){
