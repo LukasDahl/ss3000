15fecd4acac0e0df8dd5b1900863e51192df5ea6
Flotfyr27 <42938246+Flotfyr27@users.noreply.github.com>
2020-06-09 12:56:50 +0200

    Added methods to calculate and allocate points to various card moves

--- app/src/main/java/com/cdio/ss3000/DataLayer/PointCalculator.java
+++ app/src/main/java/com/cdio/ss3000/DataLayer/PointCalculator.java
@@ -1,5 +1,7 @@
 package com.cdio.ss3000.DataLayer;
 
+import java.util.ArrayList;
+
 public class PointCalculator {
 
     /*
@@ -8,35 +10,53 @@ public class PointCalculator {
      */
     private Card checkMoves(Card card){
         int temp_points = 0;
-        int i = -1;
-        for(Card move : card.getMoves()){
-            ++i;
+        //If card is an ace
+        if(card.getValue() == 1 && !card.getPlacedInFoundation()){
+            temp_points += 10;
+            card.setPoints(temp_points);
+        }
+        //Do all other moves
+        for(ArrayList<Card> move : card.getMoves()){
             //Empty spot in Tableau
-            if(move.getValue() == -1 && move.getSuit() == Suit.UNKNOWN){
+            if(move.isEmpty() && card.getValue() == 13){
                 temp_points += 3;
             }
-            //Empty spot in Foundation
-            if(move.getValue() == -2 && move.getSuit() == Suit.UNKNOWN){
-                temp_points += 5;
+            //This will be a move to foundation
+            if(move.get(move.size()-1).getSuit() == card.getSuit()){
+                temp_points += 10;
             }
+
             //Any other card in the tableau
-            if(move.getValue() > 0 && move.getValue() < 14){
+            if(move.get(move.size()-1).getValue() > 0 && move.get(move.size()-1).getValue() < 14){
                 temp_points += 2;
                 //Add points for each underlying face down card
             }
-            move.setPoints(temp_points);
+            move.get(move.size()-1).setPoints(temp_points);
         }
         Card newCard = null; //This will be used to save the final card to be returned
         int highestPoint = 0; //The current highest number of points
         for(int j = 0; j < card.getMoves().size(); j++){
+            Card currentCard = ((Card)card.getMoves().get(j).get(card.getMoves().size()-1));//Get the current card
+            int cardPoints = currentCard.getPoints();//Get points from a card
             //Compares the points to the current highest if higher -> save as new highest
-            if(card.getMoves().get(j).getPoints() > highestPoint){
-                highestPoint = card.getMoves().get(j).getPoints();
-                newCard = card.getMoves().get(j);
+            if(cardPoints > highestPoint){
+                highestPoint = cardPoints;
+                newCard = currentCard;
+            }
+            if(highestPoint < card.getPoints()){
+                highestPoint = card.getPoints();
+                newCard = card;
             }
         }
-
-        return newCard;
+        //Remove all previous moves from the supplied card
+        card.clearMoves();
+        //Save the new card (bestmove) to the list of moves (as the only one)
+        ArrayList<Card> moves = new ArrayList<>();
+        moves.add(newCard);
+        //Add the list of moves to the supplied card
+        card.addMove(moves);
+        //Return the supplied card with the single best move
+        return card;
     }
 
     public Card getBestMove(Card card){
