d5ef9751e1f850e0668d2f8270c531a5b7a2c431
Flotfyr27 <42938246+Flotfyr27@users.noreply.github.com>
2020-06-11 11:33:28 +0200

    Added baseline points for revealing cards to tests and point calc class

--- app/src/test/java/com/cdio/ss3000/DataLayer/PointCalculatorTest.java
+++ app/src/test/java/com/cdio/ss3000/DataLayer/PointCalculatorTest.java
@@ -1,5 +1,7 @@
 package com.cdio.ss3000.DataLayer;
 
+import android.widget.ArrayAdapter;
+
 import org.junit.Test;
 import java.util.ArrayList;
 
@@ -12,14 +14,19 @@ public class PointCalculatorTest {
     PointCalculator PC = new PointCalculator();
     @Test
     public void CardIsAceTest(){
+        System.out.println("\n");
         System.out.println("TEST: CardIsAceTest");
         //Build our hand of an ace of spades
         Card card = new Card(1, Suit.SPADES, true);
+        //Setup column of cards
+        ArrayList<Card> column = new ArrayList<>();
+        column.add(new Card());
+        column.add(card);
         //Create an empty move list as aces always go straight to foundation
         ArrayList<Card> moves = new ArrayList<>();
         card.addMove(moves);
         //Calculate best move
-        Card returnCard = PC.getBestMove(card);
+        Card returnCard = PC.getBestMove(column, card);
         //Return card should be the ace it self
         System.out.println("Expected result: " + card.toString());
         System.out.println("Actual result:   " + returnCard.toString());
@@ -35,17 +42,22 @@ public class PointCalculatorTest {
         System.out.println("TEST: CardIsInTableau");
         //Setting up our hand
         Card card = new Card(3, Suit.SPADES, true); //our card
+        //Setup column of cards
+        ArrayList<Card> column = new ArrayList<>();
+        column.add(new Card());
+        column.add(card);
+        //Setup move
         ArrayList<Card> moves = new ArrayList<>();
         Card moveToCard = new Card(4, Suit.HEARTS, true);//Card to move our card to
         moves.add(moveToCard);
         card.addMove(moves);//Adding possible moves to our card
-        Card returnCard = PC.getBestMove(card);//Calculate the best move receive that card as return
+        Card returnCard = PC.getBestMove(column, card);//Calculate the best move receive that card as return
         //test if the 4 of hearts is calculated to be the best move
         System.out.println("Expected result: " + moveToCard.toString());
         System.out.println("Actual result:   " + returnCard.getMoves().get(0).get(0).toString());
         assertEquals(moveToCard.getValue(), ((Card)returnCard.getMoves().get(0).get(0)).getValue());
         assertEquals(moveToCard.getSuit(), ((Card)returnCard.getMoves().get(0).get(0)).getSuit());
-        assertEquals(2, ((Card)returnCard.getMoves().get(0).get(0)).getPoints());
+        assertEquals(4, ((Card)returnCard.getMoves().get(0).get(0)).getPoints());
     }
 
     @Test
@@ -57,14 +69,18 @@ public class PointCalculatorTest {
         //Prepare possible moves
         ArrayList<Card> moves = new ArrayList<>();
         card.addMove(moves);//Add empty move
+        //Setup column of cards
+        ArrayList<Card> column = new ArrayList<>();
+        column.add(new Card());
+        column.add(card);
         //Get best move
-        Card returnCard = PC.getBestMove(card);
+        Card returnCard = PC.getBestMove(column, card);
         //Test to see if the returned card is a king of hearts
         System.out.println("Expected result: " + card.toString());
         System.out.println("Actual result:   " + returnCard.toString());
         assertEquals(card.getValue(), returnCard.getValue());
         assertEquals(card.getSuit(), returnCard.getSuit());
-        assertEquals(3, returnCard.getPoints());
+        assertEquals(5, returnCard.getPoints());
     }
 
     @Test
@@ -82,15 +98,19 @@ public class PointCalculatorTest {
         //Add moves to king
         king.addMove(foundation);
         king.addMove(tableauSpace);
+        //Setup column of cards
+        ArrayList<Card> column = new ArrayList<>();
+        column.add(new Card());
+        column.add(king);
         //Check the move
-        Card bestmove = PC.getBestMove(king);
+        Card bestmove = PC.getBestMove(column, king);
         //Best move should be to move to the queen in the foundation
         System.out.println("Expected result: " + queen.toString());
         System.out.println("Actual result:   " + ((Card)bestmove.getMoves().get(0).get(0)).toString());
         //Check to see if we have received the correct card
         assertEquals(queen.getValue(), ((Card)bestmove.getMoves().get(0).get(0)).getValue());
         assertEquals(queen.getSuit(), ((Card)bestmove.getMoves().get(0).get(0)).getSuit());
-        assertEquals(10, ((Card)bestmove.getMoves().get(0).get(0)).getPoints());
+        assertEquals(12, ((Card)bestmove.getMoves().get(0).get(0)).getPoints());
 
     }
 
