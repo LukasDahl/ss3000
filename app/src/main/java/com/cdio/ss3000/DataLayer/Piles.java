3a9b2c4740e1024d84b8d6a368fec9323c69ebe6
Jens.Agersgaard <alex.norgaard@gmail.com>
2020-06-11 12:15:28 +0200

    Piles class finished

--- app/src/main/java/com/cdio/ss3000/DataLayer/Piles.java
+++ app/src/main/java/com/cdio/ss3000/DataLayer/Piles.java
@@ -3,8 +3,37 @@ package com.cdio.ss3000.DataLayer;
 import java.util.ArrayList;
 
 public class Piles {
-    private ArrayList<Card> stack;
+    private ArrayList<Card> stock, waste;
+    private ArrayList<Card> knownCards = new ArrayList<>();
 
-   // public Piles();
+    public Piles(ArrayList<Card> stock, ArrayList<Card> waste){
+        this.stock = stock;
+        this.waste = waste;
 
+        if(!waste.isEmpty()) knownCards.addAll(waste);
+
+    }
+
+    public void addCard(Card wasteTop){
+        if(!knownCards.contains(wasteTop)){
+            knownCards.add(wasteTop);
+        }
+        waste.add(wasteTop);
+        stock.remove(wasteTop);
+
+    }
+
+    public void removeCard(Card wasteTop){
+        waste.remove(wasteTop);
+        knownCards.remove(wasteTop);
+    }
+
+    public ArrayList<Card> getKnownCards(){
+        return knownCards;
+    }
+
+    public void allCardsToStock(){
+        stock = waste;
+        waste.clear();
+    }
 }
