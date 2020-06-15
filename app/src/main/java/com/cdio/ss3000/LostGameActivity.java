98a1672c3a98f2a094216795d0f90fc5962b5aba
kajgaard <maria.kajgaard@gmail.com>
2020-06-15 14:52:54 +0200

    Added LostGameActivity

---
+++ app/src/main/java/com/cdio/ss3000/LostGameActivity.java
@@ -0,0 +1,13 @@
+package com.cdio.ss3000;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LostGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lost_game);
+    }
+}
